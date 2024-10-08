833810e46ba9339ea25ef4aaac4d5f0311aa4a72
hanasa2023 <hanasakayui2022@gmail.com>
2024-10-08 20:51:29 +0800

    🔖 更新版本

--- nonebot_plugin_updater/__init__.py
+++ nonebot_plugin_updater/__init__.py
@@ -4,13 +4,20 @@ from nonebot.plugin import PluginMetadata
 from .commands import check_update, g_plugin_list, update_plugin
 from .config import Config
 
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 __plugin_meta__ = PluginMetadata(
-    name='nonebot-plugin-updater',
+    name='nb插件更新器',
     description='一款全新的检测已安装插件更新情况的插件',
     type='application',
     homepage='https://github.com/hanasa2023/nonebot-plugin-updater#readme',
-    usage='',
+    usage="""
+|      指令      | 权限 | 需要@ |                                       说明                                       |                  示例                  |
+| :------------: | :--: | :---: | :------------------------------------------------------------------------------: | :------------------------------------: |
+| `获取插件列表` |  无  |  无   |                               获取已安装的插件列表                               |            `/获取插件列表`             |
+| `检查插件更新` |  无  |  无   |                                检查可用的插件更新                                |            `/检查插件更新`             |
+|   `更新插件`   |  无  |  无   | 更新已安装的插件，若需只更新单个插件，则指令为`更新插件 name <需要更新的插件名>` | `/更新插件 name nonebot-pluign-status` |
+|    `关闭nb`    |  无  |  无   |                                  远程关闭 nb nb                                  |               `/关闭nb`                |
+|    `重启nb`    |  无  |  无   |                                  远程重启 nb nb                                  |               `/重启nb`                |""",
     config=Config,
     extra={
         'version': __version__,
